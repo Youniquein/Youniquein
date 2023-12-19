c0cde408dd71b9dbcb9abe337834c398ce31aa61
Youniquein <youniquein@gmail.com>
2023-12-19 14:41:32 +0900

    CSV

---
+++ Y_project/Assets/Scripts/Monster.cs
@@ -0,0 +1,23 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Monster : MonoBehaviour
+{
+    public int id;
+    public int hp;
+    public int atkPower;
+    public int def;
+    public int cri;
+
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
